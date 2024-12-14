5ca230cf62fd2b992cb5cd45c39bcfb8a82b1b38
gutche <gledrianregalagutierrez@gmail.com>
2024-12-14 01:41:10 +0100

    styles: change buttons on hover color

--- frontend/src/components/Notification.vue
+++ frontend/src/components/Notification.vue
@@ -3,8 +3,8 @@
 		<img class="item1" src="/no-profile.png" alt="User's profile picture" />
 		<p class="item2">Lil bro sent you a friend request</p>
 		<div class="item3">
-			<button>Accept</button>
-			<button>Decline</button>
+			<button class="accept" @click="accept">Accept</button>
+			<button class="decline" @click="decline">Decline</button>
 		</div>
 	</div>
 </template>
@@ -12,20 +12,25 @@
 const props = defineProps({
 	user: Object,
 });
+const accept = () => {};
+const decline = () => {};
 </script>
 <style scoped>
 .grid-container {
+	padding: 5px;
+	margin-top: 5px;
 	display: grid;
 	grid-template-columns: 1fr 3fr;
 	grid-template-rows: auto;
-	gap: 5px;
 }
 
 .item1 {
 	grid-column: 1;
 	grid-row: 1 / span 2;
-	width: 80px;
-	height: 80px;
+	width: 65px;
+	height: 65px;
+	align-self: center;
+	justify-self: center;
 }
 
 .item2 {
@@ -35,7 +40,7 @@ const props = defineProps({
 	font-size: 16px;
 	font-weight: bold;
 	color: #333;
-	margin-bottom: 0;
+	margin: 0;
 }
 
 .item3 {
@@ -48,7 +53,7 @@ const props = defineProps({
 }
 
 button {
-	padding: 10px;
+	padding: 8px;
 	font-size: 12px;
 	font-weight: 500;
 	color: #000;
@@ -62,10 +67,18 @@ button {
 }
 
 button:hover {
-	background-color: #23c483;
-	box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
 	color: #fff;
 	transform: translateY(-7px);
+}
+
+.accept:hover {
+	background-color: #2aa876;
+	box-shadow: 0px 15px 20px rgba(42, 168, 118, 0.4);
+}
+
+.decline:hover {
+	background-color: #a8323c;
+	box-shadow: 0px 15px 20px rgba(168, 50, 60, 0.4);
 }
 
 button:active {
