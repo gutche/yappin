857d20bb0c6daea6733f3ce2d3acf93469038637
gutche <gledrianregalagutierrez@gmail.com>
2025-02-03 00:01:34 +0100

    fix redis env

--- server/configs/redis.config.js
+++ server/configs/redis.config.js
@@ -1,9 +1,16 @@
 import Redis from "ioredis";
 import { RedisMessageStore } from "../store/message.store.js";
 
-const redisClient = new Redis();
+const { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT } = process.env;
+
+const redisClient = new Redis({
+	host: REDIS_HOST,
+	password: REDIS_PASSWORD,
+	port: REDIS_PORT,
+});
+
 redisClient.on("connect", () => {
-	console.log("Connected to redis successfully");
+	console.log("✅ Connected to redis successfully");
 });
 
 const messageStore = new RedisMessageStore(redisClient);
