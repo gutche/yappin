18e3bb1b3949dd14109df15ece6a72d7d6977be1
gutche <gledrianregalagutierrez@gmail.com>
2025-02-03 15:45:02 +0100

    im stupid

--- webapp/src/api/useFetch.js
+++ webapp/src/api/useFetch.js
@@ -2,7 +2,7 @@ import { createFetch } from "@vueuse/core";
 
 const API_URL = import.meta.env.VITE_API_URL;
 const useFetch = createFetch({
-	baseUrl: `${API_URL} + /api`,
+	baseUrl: `${API_URL}/api`,
 	fetchOptions: {
 		credentials: "include",
 	},
